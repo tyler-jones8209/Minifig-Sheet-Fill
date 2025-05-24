79e1369ba7eaf7382571ccce1f26309f251d86ea
Tyler Jones <tdjones1980@yahoo.com>
2025-05-23 20:03:25 -0400

    Added game plan baby

---
+++ minifig_sheet_fill.py
@@ -0,0 +1,9 @@
+# Use bs4 and selenium to:
+# 1. Log into my BrickLink account (super easy)
+# 2. Navigate to 'My Collection' page (easy)
+# 3. Create a list of all item id's (e.g. njo0047) on all the pages of my collection (difficult)
+# 4. Using id's, save name, id, release date, price*, quantity, and notes (moderate difficulty)
+# 5. Scrape average used current price from each minifig page using previously written script (easy)
+# 6. Save all information into a list (easy)
+# 7. Possibly turn information into CSV (moderate difficulty)
+# 8. Populate Goolge Sheets sheet with all the scraped information for a beautiful result (difficult)
