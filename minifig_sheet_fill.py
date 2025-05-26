ecc954589a3b40bf0e6bbea8ffa12fdc50406548
Tyler Jones <tdjones1980@yahoo.com>
2025-05-25 21:59:30 -0400

    Put some things into function and added CSV writing

--- minifig_sheet_fill.py
+++ minifig_sheet_fill.py
@@ -35,7 +35,48 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import csv
 
-start = time.perf_counter()
+def login(driver, username, password):
+    # load login page
+    url = "https://store.bricklink.com/v2/login.page?menu=Home&logInTo=https%3A%2F%2Fstore.bricklink.com%2Fv2%2Fglobalcart.page"
+    driver.get(url)
+
+    # wait for cookie popup and destroy that shit (using JS because it didn't work the other way)
+    cookie_btn = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//button[text()='Just necessary']"))
+    )
+    driver.execute_script("arguments[0].click();", cookie_btn)
+
+    # fill username and password fields
+    username_field = driver.find_element(By.ID, 'frmUsername')
+    password_field = driver.find_element(By.ID, 'frmPassword')
+    driver.find_element(By.ID, 'blbtnLogin')
+    username_field.send_keys(username)
+    password_field.send_keys(password)
+
+    # click login button
+    driver.find_element(By.ID, 'blbtnLogin').click()
+
+def get_total_pages(driver):
+    # load the first My Collection page initially to retrieve number of total pages
+    driver.get("https://www.bricklink.com/v3/myCollection/main.page?q=&itemType=M&page=1")
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    time.sleep(0.5)
+
+    # scrape total number of pages for looping
+    pagination_jump = soup.find('label', class_='pagination__jump')
+
+    # setting page_amount to 1 as a default means if the pagination jump is missing, it only page one gets added to the list of pages to scrape
+    page_amount = 1
+
+    page_numbers = []
+    if pagination_jump:
+        pagination_jump = pagination_jump.text.strip()
+        page_amount = int(pagination_jump.split('/')[1].strip())
+    for x in range(1, page_amount + 1):
+        page_numbers.append(x)
+
+    return page_numbers
 
 # function to scrape price (based on listed condition) and release year from minifig specific pages
 def scrape_release_and_price(driver, identifier, condition):
@@ -87,148 +128,130 @@ def scrape_release_and_price(driver, identifier, condition):
 
     return price, release_year
 
-# select web driver and set to 'headless' so it doesn't display while it runs (though it is super sick to see when not headless)
-options = webdriver.ChromeOptions()
-#options.add_argument('--headless')
-driver = webdriver.Chrome(options=options)
+def scrape_minifig_info(driver, page_numbers):
+    # this houses all of the scraped info; each item is a list
+    minifig_info = []
 
-# load login page
-url = "https://store.bricklink.com/v2/login.page?menu=Home&logInTo=https%3A%2F%2Fstore.bricklink.com%2Fv2%2Fglobalcart.page"
-driver.get(url)
+    # loop through all pages containing minifigs in My Collection
+    for page in page_numbers:
+        driver.get(f"https://www.bricklink.com/v3/myCollection/main.page?q=&itemType=M&page={str(page)}")
 
-# wait for cookie popup and destroy that shit (using JS because it didn't work the other way)
-cookie_btn = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//button[text()='Just necessary']"))
-)
-driver.execute_script("arguments[0].click();", cookie_btn)
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'listItemView-0'))
+        )  
 
-# load data stored in .env file
-load_dotenv()
-user, passwd = os.getenv("UNAME"), os.getenv("PASSWD")
+        # get data for each visited page
+        minifig_soup = BeautifulSoup(driver.page_source, 'html.parser')  
 
-# fill username and password fields
-username_field = driver.find_element(By.ID, 'frmUsername')
-password_field = driver.find_element(By.ID, 'frmPassword')
-driver.find_element(By.ID, 'blbtnLogin')
-username_field.send_keys(user)
-password_field.send_keys(passwd)
+        # get any data under elements that contain the id listItemView-XX (e.g., listItemView-0, listItemView-23)
+        list_items = minifig_soup.find_all(id=re.compile('^listItemView-\d+$'))
+        for item in list_items:
+            item_id = item.get_attribute_list('id')
 
-# click login button
-driver.find_element(By.ID, 'blbtnLogin').click()
+            # get data for each listViewItem-XX id
+            list_item = minifig_soup.find(id=str(item_id[0]))
 
-time.sleep(0.5)
+            # scrape minifig name
+            name = list_item.find('div', class_='text--bold l-cursor-pointer')
+            name = name.text.strip()
+            if name is None:
+                name = ""
 
-# load the first My Collection page initially to retrieve number of total pages
-driver.get("https://www.bricklink.com/v3/myCollection/main.page?q=&itemType=M&page=1")
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-time.sleep(0.5)
-
-# scrape total number of pages for looping
-pagination_jump = soup.find('label', class_='pagination__jump')
-
-# setting page_amount to 1 as a default means if the pagination jump is missing, it only page one gets added to the list of pages to scrape
-page_amount = 1
-page_numbers = []
-if pagination_jump:
-    pagination_jump = pagination_jump.text.strip()
-    page_amount = int(pagination_jump.split('/')[1].strip())
-for x in range(1, page_amount + 1):
-    page_numbers.append(x)
-
-# this houses all of the scraped info; each item is a list
-minifig_info = []
-
-# loop through all pages containing minifigs in My Collection
-for page in page_numbers:
-    driver.get(f"https://www.bricklink.com/v3/myCollection/main.page?q=&itemType=M&page={str(page)}")
-
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, 'listItemView-0'))
-    )  
-
-    # get data for each visited page
-    minifig_soup = BeautifulSoup(driver.page_source, 'html.parser')  
-
-    # get any data under elements that contain the id listItemView-XX (e.g., listItemView-0, listItemView-23)
-    list_items = minifig_soup.find_all(id=re.compile('^listItemView-\d+$'))
-    for item in list_items:
-        item_id = item.get_attribute_list('id')
-
-        # get data for each listViewItem-XX id
-        list_item = minifig_soup.find(id=str(item_id[0]))
-
-        # scrape minifig name
-        name = list_item.find('div', class_='text--bold l-cursor-pointer')
-        name = name.text.strip()
-        if name is None:
-            name = ""
-
-        # scrape intellectual property minifig (e.g., Ninjago, Indiana Jones, etc)
-        # scrape subcategory of IP (e.g., NINJAGO: Rise of the Snakes, Star Wars: Star Wars Episode 1, Super Heroes: Batman II)
-        ip_text = list_item.find('div', class_='personal-inventory__item-category')
-        ip_text = ip_text.text.strip()
-        parts   = [p.strip() for p in ip_text.split(':', 1)]
-        if len(parts) == 1:
-            ip, category = parts[0], ""
-        else:
-            ip, category = parts[0], parts[1]
-
-        # scrape BrickLink item number (e.g., njo0047, sw0002, sh0041)
-        identifier = list_item.find('div', class_='personal-inventory__list-item-list-cell--item-no text--break-word l-cursor-pointer')
-        identifier = identifier.text.strip()
-        if identifier is None:
-            identifier = ""
-
-        # scrape chosen condition of minifig (used or new)
-        condition = list_item.find('div', class_='personal-inventory__list-item-list-cell--cond')
-        condition = condition.text.strip()
-        if condition is None:
-            condition = ""
-
-        # scrape price and release year from a different page using a function
-        price, release_year = scrape_release_and_price(driver, identifier, condition)
-        if price is None:
-            price = ""
-        if release_year is None:
-            release_year = ""
-
-        # scrape quantity of minifigs in collection
-        qty_container = list_item.find('div', class_='personal-inventory__list-item-list-cell--qty')
-        if qty_container:
-            input_tag = qty_container.find('input', class_='text-input text--center personal-inventory__list-qty')
-            if input_tag and input_tag.has_attr('value'):
-                quantity = input_tag['value']
+            # scrape intellectual property minifig (e.g., Ninjago, Indiana Jones, etc)
+            # scrape subcategory of IP (e.g., NINJAGO: Rise of the Snakes, Star Wars: Star Wars Episode 1, Super Heroes: Batman II)
+            ip_text = list_item.find('div', class_='personal-inventory__item-category')
+            ip_text = ip_text.text.strip()
+            parts   = [p.strip() for p in ip_text.split(':', 1)]
+            if len(parts) == 1:
+                ip, category = parts[0], ""
             else:
-                print("Input tag not found or missing value attribute.")
+                ip, category = parts[0], parts[1]
+
+            # scrape BrickLink item number (e.g., njo0047, sw0002, sh0041)
+            identifier = list_item.find('div', class_='personal-inventory__list-item-list-cell--item-no text--break-word l-cursor-pointer')
+            identifier = identifier.text.strip()
+            if identifier is None:
+                identifier = ""
+
+            # scrape chosen condition of minifig (used or new)
+            condition = list_item.find('div', class_='personal-inventory__list-item-list-cell--cond')
+            condition = condition.text.strip()
+            if condition is None:
+                condition = ""
+
+            # scrape price and release year from a different page using a function
+            price, release_year = scrape_release_and_price(driver, identifier, condition)
+            if price is None:
+                price = ""
+            if release_year is None:
+                release_year = ""
+
+            # scrape quantity of minifigs in collection
+            qty_container = list_item.find('div', class_='personal-inventory__list-item-list-cell--qty')
+            if qty_container:
+                input_tag = qty_container.find('input', class_='text-input text--center personal-inventory__list-qty')
+                if input_tag and input_tag.has_attr('value'):
+                    quantity = input_tag['value']
+                else:
+                    print("Input tag not found or missing value attribute.")
+                    quantity = ""
+            else:
+                print("Quantity container not found.")
                 quantity = ""
-        else:
-            print("Quantity container not found.")
-            quantity = ""
 
-        # scrape and transfer over any added notes
-        notes_div = list_item.find('div', class_='personal-inventory__cell--note l-margin-top--sm personal-inventory__note-field')
-        if notes_div:
-            direct_text = notes_div.find(string=True, recursive=False)
-            if direct_text:
-                notes = direct_text.strip()
+            # scrape and transfer over any added notes
+            notes_div = list_item.find('div', class_='personal-inventory__cell--note l-margin-top--sm personal-inventory__note-field')
+            if notes_div:
+                direct_text = notes_div.find(string=True, recursive=False)
+                if direct_text:
+                    notes = direct_text.strip()
+                else:
+                    notes = "Add Notes"
             else:
-                notes = "Add Notes"
-        else:
-            notes = "Error parsing notes."
+                notes = "Error parsing notes."
 
-        minifig_info.append([name, identifier, ip, category, release_year, condition, price, quantity, notes])
+            minifig_info.append([name, identifier, ip, category, release_year, condition, price, quantity, notes])
 
-time.sleep(10)
+    return minifig_info
 
+def write_to_csv(minifig_info):
+    with open('scraped_minifig_info.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['name', 'id', 'ip', 'category', 'release', 'condition', 'price', 'quantity', 'notes'])
+        writer.writerows(minifig_info)
 
-#for x in minifig_info:
-#    print(x)
-           
-driver.quit()
+def main():
+    start = time.perf_counter()
 
-end = time.perf_counter()
+    # select web driver and set to 'headless' so it doesn't display while it runs (though it is super sick to see when not headless)
+    options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
 
-# need to figure out if I can reduce runtime
-# runs on my collection (65 entries) took: run1: 68.67s, run2: 78.40s, run3: 77.50s, run4: 75.12s, run5: 72.91s
-print(f"Runtime: {end - start}s")
+    # load data stored in .env file
+    load_dotenv()
+    user, passwd = os.getenv("UNAME"), os.getenv("PASSWD")
+
+    login(driver=driver, username=user, password=passwd)
+
+    time.sleep(0.5)
+    
+    page_numbers = get_total_pages(driver=driver)
+
+    minifig_info = scrape_minifig_info(driver=driver, page_numbers=page_numbers)
+
+    time.sleep(10)
+            
+    driver.quit()
+
+    # works but need to fix it so that names with inner quotes don't keep the added outer quotes
+    write_to_csv(minifig_info)
+
+    end = time.perf_counter()
+
+    # need to figure out if I can reduce runtime
+    # runs on my collection (65 entries) took: run1: 68.67s, run2: 78.40s, run3: 77.50s, run4: 75.12s, run5: 72.91s
+    print(f"Runtime: {end - start}s")
+
+if __name__ == '__main__':
+    main()
